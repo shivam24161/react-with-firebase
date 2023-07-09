5ace95b6a25b3bb34f8556b8084e08a1a23e148d
shivam24161 <shivam24161@gmail.com>
2023-07-09 11:19:10 +0530

    chat app using firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAslctXFkxmrmcSxaR5G3YdnyOxgC236UQ",
+  authDomain: "wearefriendsfrombiet.firebaseapp.com",
+  databaseURL: "https://wearefriendsfrombiet-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wearefriendsfrombiet",
+  storageBucket: "wearefriendsfrombiet.appspot.com",
+  messagingSenderId: "790387619583",
+  appId: "1:790387619583:web:d2bf8818c193ff30c8beca"
+}
+// Initialize Firebase
+initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
