9dcc4597ee6f2b80b665b4a5dfaf2659e241319a
shivam24161 <shivam24161@gmail.com>
2023-07-09 12:13:05 +0530

    chat app using firebase, mui implemented

--- src/App.js
+++ src/App.js
@@ -1,91 +1,101 @@
-import { useEffect, useState } from 'react';
-import { getDatabase, push, ref, set,onChildAdded } from "firebase/database";
+import { useEffect, useState } from "react";
+import { getDatabase, push, ref, set, onChildAdded } from "firebase/database";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
-import './App.css';
+import "./App.css";
+import { Button, TextField } from "@mui/material";
 
 function App() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [chats, setChats] = useState([]);
-  const [msg, setMsg] = useState('');
-  const [error,setError]=useState('')
+  const [msg, setMsg] = useState("");
+  const [error, setError] = useState("");
   const db = getDatabase();
-  const chatListRef = ref(db, 'chats');
+  const chatListRef = ref(db, "chats");
 
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
 
-  const googleLogin = () =>{
+  const googleLogin = () => {
     signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    // const credential = GoogleAuthProvider.credentialFromResult(result);
-    // const token = credential.accessToken;
-    // // The signed-in user info.
-    // const user = result.user;
-    setUser({name:result.user.displayName, email: result.user.email})
-  }).catch((error) => {
-    setError(error)
-  });
-  }
-  
-  const updateHeight=()=>{
-    const el = document.getElementById('chat');
-    if(el){
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // // The signed-in user info.
+        // const user = result.user;
+        setUser({ name: result.user.displayName, email: result.user.email });
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
       el.scrollTop = el.scrollHeight;
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     onChildAdded(chatListRef, (data) => {
-      setChats(chats=>[...chats,data.val()])
-      setTimeout(()=>{
-        updateHeight()
-      },100)
+      setChats((chats) => [...chats, data.val()]);
+      setTimeout(() => {
+        updateHeight();
+      }, 100);
     });
-  },[])
+  }, []);
 
   const sendChat = () => {
     const chatRef = push(chatListRef);
     set(chatRef, {
-      user, message: msg 
+      user,
+      message: msg,
     });
-    setMsg('');
+    setMsg("");
   };
   return (
     <>
-    {error.length > 2 ? <div>{error}</div>:<div>
-      {user.email? null: <div>
-        {/* <input
-          type="text"
-          placeholder="Enter user to start"
-          onBlur={(e) => setUser(e.target.value)}
-        ></input> */}
-        <button onClick={e=>{googleLogin()}}>Google SignIn</button>
-      </div>}
-   { user.email? <div>
-      <h3>User: {user.name}</h3>
-      <div id="chat" className="chat-container">
-        {chats.map((c,i) => (
-          <div key={i} className={`container ${c.user.email === user.email ? 'me' : ''}`}>
-            <p className="chatbox">
-              <strong>{c.user.name}: </strong>
-              <span>{c.message}</span>
-            </p>
-          </div>
-        ))}
-      </div>
-      <div className="btm">
-        <input
-          type="text"
-          onInput={(e) => setMsg(e.target.value)}
-          value={msg}
-          placeholder="enter your chat"
-        ></input>
-        <button onClick={(e) => sendChat()}>Send</button>
-      </div>
-      </div> : null}
-      </div>}
+      {error.length > 2 ? (
+        <div>{error}</div>
+      ) : (
+        <div className="main-container">
+          {user.email ? null : (
+            <div className="google-signin">
+              <Button variant="outlined" onClick={() => googleLogin()}>
+                Sign in With Google
+              </Button>
+            </div>
+          )}
+          {user.email ? (
+            <div>
+              <h3>User: {user.name}</h3>
+              <div id="chat" className="chat-container">
+                {chats.map((c, i) => (
+                  <div
+                    key={i}
+                    className={`container ${
+                      c.user.email === user.email ? "me" : ""
+                    }`}
+                  >
+                    <p className="chatbox">
+                      <strong>{c.user.name}: </strong>
+                      <span>{c.message}</span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+              <div className="btm">
+              <TextField label="Type your message" fullWidth  variant="outlined" onInput={(e) => setMsg(e.target.value)}/>
+                <Button variant="contained" onClick={(e) => sendChat()}>
+                  Send
+                </Button>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      )}
     </>
   );
 }
